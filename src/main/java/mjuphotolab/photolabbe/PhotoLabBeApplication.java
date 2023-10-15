ef8e9e21f5cf5baf3ec77c344341dcb4d868484c
나경호 <gangho324@gmail.com>
2023-10-15 20:39:22 +0900

    Initial commit

---
+++ src/main/java/mjuphotolab/photolabbe/PhotoLabBeApplication.java
@@ -0,0 +1,13 @@
+package mjuphotolab.photolabbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoLabBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoLabBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mjuphotolab/photolabbe/PhotoLabBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mjuphotolab.photolabbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoLabBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
