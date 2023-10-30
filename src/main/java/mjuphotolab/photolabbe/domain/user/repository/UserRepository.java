1a1dbcffaacf6420d38c01d0ed1fb50415cfa9aa
나경호 <gangho324@gmail.com>
2023-10-30 23:00:42 +0900

    feat, refactor: UserRepository 생성 및 회원가입 기능 추가

---
+++ src/main/java/mjuphotolab/photolabbe/domain/user/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package mjuphotolab.photolabbe.domain.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import mjuphotolab.photolabbe.domain.user.entity.SocialType;
+import mjuphotolab.photolabbe.domain.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByOauthId(final String oauthId);
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByNickname(String refreshToken);
+
+	Optional<User> findByRefreshToken(String refreshToken);
+
+	Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String id);
+}
