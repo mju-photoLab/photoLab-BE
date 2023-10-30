55022b4a438b55421b4db0e8dbc4702a25b4854b
나경호 <gangho324@gmail.com>
2023-10-30 23:02:43 +0900

    feat, refactor: 회원 정보 수정 dto 생성

---
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/dto/request/UpdateUserRequest.java
@@ -0,0 +1,23 @@
+package mjuphotolab.photolabbe.domain.user.controller.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UpdateUserRequest {
+
+	@NotBlank(message = "이름을 입력해주세요.")
+	@Size(min = 2, max = 10, message = "이름은 2자에서 10자 사이여야합니다.")
+	private String nickname;
+
+	@NotBlank(message = "이메일을 입력해주세요.")
+	private String email;
+
+	@Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야합니다.")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$", message = "비밀번호는 문자, 숫자, 기호가 1개 이상 포함되어야합니다.")
+	private String password;
+}
