7aee5e0d09056288768689241ca58cda057e3b93
나경호 <gangho324@gmail.com>
2023-11-02 00:07:09 +0900

    refactor: 회원가입 api

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package mjuphotolab.photolabbe.domain.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,14 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/sign-up")
-	public void signUp(@RequestBody UserSignUpDto userSignUpDto) {
+	public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
 		userService.signUp(userSignUpDto);
+		return "회원가입 성공";
 	}
+
+	@GetMapping("/jwt-test")
+	public String jwtTest() {
+		return "jwtTest 요청 성공";
+	}
+
 }
