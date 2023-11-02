0de7769a14921d0c80aefdc5697287367c355d81
나경호 <gangho324@gmail.com>
2023-11-03 01:30:15 +0900

    feat: 생성, 수정일 entity

---
+++ src/main/java/mjuphotolab/photolabbe/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package mjuphotolab.photolabbe.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	private LocalDateTime createdDateTime;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedDateTime;
+}
