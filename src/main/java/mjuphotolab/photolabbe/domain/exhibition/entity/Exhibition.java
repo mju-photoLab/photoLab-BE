a7bb3ba61a91c370bbd79f8da58055be94405ca6
fuirian <kmjkhj2017@gmail.com>
2023-11-02 17:28:35 +0900

    refactor: Exhibition id 추가

--- src/main/java/mjuphotolab/photolabbe/domain/exhibition/entity/Exhibition.java
+++ src/main/java/mjuphotolab/photolabbe/domain/exhibition/entity/Exhibition.java
@@ -1,7 +1,13 @@
 package mjuphotolab.photolabbe.domain.exhibition.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-// @Entity
+@Entity
 public class Exhibition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
