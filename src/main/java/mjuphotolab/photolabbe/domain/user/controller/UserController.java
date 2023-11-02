59a6f4fb6c9591881a7c4cdb6e0aa75a219a2acb
나경호 <gangho324@gmail.com>
2023-11-03 01:43:51 +0900

    refactor: validation 추가

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mjuphotolab.photolabbe.domain.user.controller.dto.request.UserSignUpDto;
 import mjuphotolab.photolabbe.domain.user.service.UserService;
@@ -16,14 +17,18 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/sign-up")
-	public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
+	public String signUp(@Valid @RequestBody UserSignUpDto userSignUpDto) throws Exception {
 		userService.signUp(userSignUpDto);
 		return "회원가입 성공";
 	}
+
+	// @GetMapping("/api/users/{userId}/awards")
 
 	@GetMapping("/jwt-test")
 	public String jwtTest() {
 		return "jwtTest 요청 성공";
 	}
+
+
 
 }
