01f452600cb6903de19b60e04c0ac5bef86b83d6
나경호 <gangho324@gmail.com>
2023-11-03 01:30:32 +0900

    feat: Competition 생성

--- src/main/java/mjuphotolab/photolabbe/domain/competition/entity/Competition.java
+++ src/main/java/mjuphotolab/photolabbe/domain/competition/entity/Competition.java
@@ -1,7 +1,38 @@
 package mjuphotolab.photolabbe.domain.competition.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mjuphotolab.photolabbe.common.BaseEntity;
+import mjuphotolab.photolabbe.domain.user.entity.User;
 
-// @Entity
-public class Competition {
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Competition extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private String content;
+	private int awards;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	@Builder
+	private Competition(final String title, final String content, final int awards) {
+		this.title = title;
+		this.content = content;
+		this.awards = awards;
+	}
 }
