ffbd1615b485b9b5ee5d58d22a186ee45561b5bc
나경호 <gangho324@gmail.com>
2023-11-03 03:01:59 +0900

    feat: admin 권한 부여

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package mjuphotolab.photolabbe.domain.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +19,8 @@ public class UserController {
 	private final UserService userService;
 
 	@PostMapping("/sign-up")
-	public String signUp(@Valid @RequestBody UserSignUpDto userSignUpDto) throws Exception {
+	public void signUp(@Valid @RequestBody UserSignUpDto userSignUpDto) throws Exception {
 		userService.signUp(userSignUpDto);
-		return "회원가입 성공";
 	}
 
 	// @GetMapping("/api/users/{userId}/awards")
@@ -29,6 +30,10 @@ public class UserController {
 		return "jwtTest 요청 성공";
 	}
 
-
+	// TODO: 최종적으로는 admin이 붙어야함
+	@PatchMapping("/api/users/{userId}/editRole")
+	public Long updateRoleToAdmin(@PathVariable Long userId) {
+		return userService.setAdmin(userId);
+	}
 
 }
