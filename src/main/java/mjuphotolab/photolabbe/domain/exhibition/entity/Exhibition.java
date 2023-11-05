e65729ff0d1437c1821feb79ffa8bc5749034e4c
fuirian <kmjkhj2017@gmail.com>
2023-11-05 22:23:35 +0900

    refactor: Exhibition import @ManyToOne 수정

--- src/main/java/mjuphotolab/photolabbe/domain/exhibition/entity/Exhibition.java
+++ src/main/java/mjuphotolab/photolabbe/domain/exhibition/entity/Exhibition.java
@@ -1,6 +1,7 @@
 package mjuphotolab.photolabbe.domain.exhibition.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,18 +11,19 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import mjuphotolab.photolabbe.common.BaseEntity;
 import mjuphotolab.photolabbe.domain.user.entity.User;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Exhibition {
+public class Exhibition extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")  // user_id를 외래 키로 사용
     private User user;
 
