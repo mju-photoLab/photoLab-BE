bfc32727a3183ca66532416d7e6e9dada0321fa7
나경호 <gangho324@gmail.com>
2023-11-06 22:50:12 +0900

    refactor: UserController 세분화

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserController.java
@@ -1,39 +1,35 @@
 package mjuphotolab.photolabbe.domain.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import mjuphotolab.photolabbe.domain.user.controller.dto.request.UserSignUpDto;
+import mjuphotolab.photolabbe.common.LoginUser;
+import mjuphotolab.photolabbe.domain.user.entity.User;
 import mjuphotolab.photolabbe.domain.user.service.UserService;
 
-@RestController
+@Controller
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
 
 	private final UserService userService;
 
-	@PostMapping("/sign-up")
-	public void signUp(@Valid @RequestBody UserSignUpDto userSignUpDto) throws Exception {
-		userService.signUp(userSignUpDto);
-	}
+	/**
+	 * 학번으로 찾아야할 듯
+	 */
+	// @GetMapping("/users/awards")
+	// public AwardsResponse userAwards(@AuthenticationPrincipal User user) {
+	// 	userService.getAwards(user);
+	// }
 
-	// @GetMapping("/api/users/{userId}/awards")
-
-	@GetMapping("/jwt-test")
-	public String jwtTest() {
-		return "jwtTest 요청 성공";
-	}
-
-	// TODO: 최종적으로는 admin이 붙어야함
-	@PatchMapping("/api/users/{userId}/editRole")
-	public Long updateRoleToAdmin(@PathVariable Long userId) {
-		return userService.setAdmin(userId);
-	}
+	/**
+	 * 초기 어드민 권한 부여를 위함
+	 */
+	// @PostMapping("/first")
+	// public void firstSet(@LoginUser User user) {
+	// 	userService.setAdmin(user.getId());
+	// }
 
 }
