01baa0cdeb89335a939a90cd949b6d4739148adf
나경호 <gangho324@gmail.com>
2023-11-06 22:38:11 +0900

    refactor: 어드민 UserController로 분할

---
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/AdminUserController.java
@@ -0,0 +1,28 @@
+package mjuphotolab.photolabbe.domain.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import mjuphotolab.photolabbe.common.LoginUser;
+import mjuphotolab.photolabbe.domain.user.entity.User;
+import mjuphotolab.photolabbe.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("api/admin/users")
+@RequiredArgsConstructor
+public class AdminUserController {
+
+	private final UserService userService;
+	@PatchMapping("/editRole")
+	public Long updateRoleToAdmin(@LoginUser User user) {
+		return userService.setAdmin(user.getId());
+	}
+
+	@GetMapping("/jwt-test")
+	public String jwtTest() {
+		return "jwtTest 요청 성공";
+	}
+}
