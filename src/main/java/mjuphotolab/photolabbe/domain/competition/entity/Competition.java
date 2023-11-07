e1ccfc847d7ef4bfb7a16f70bc6f7bacdb841d8f
나경호 <gangho324@gmail.com>
2023-11-08 01:19:35 +0900

    refactor: 컬럼에 id 값 지정

--- src/main/java/mjuphotolab/photolabbe/domain/competition/entity/Competition.java
+++ src/main/java/mjuphotolab/photolabbe/domain/competition/entity/Competition.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
@@ -26,6 +28,7 @@ public class Competition extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "competition_id")
 	private Long id;
 
 	private String title;
@@ -33,6 +36,7 @@ public class Competition extends BaseEntity {
 	private int awardCount;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
 	private User user;
 
 	@OneToMany(mappedBy = "competition", cascade = CascadeType.ALL)
