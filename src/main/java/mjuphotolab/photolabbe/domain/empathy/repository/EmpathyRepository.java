80ac972a68b5db19478bebdbf5e63668ec478950
taewook02 <taewook02@mju.ac.kr>
2023-11-07 17:25:07 +0900

    feat: Empathy Repository 추가

---
+++ src/main/java/mjuphotolab/photolabbe/domain/empathy/repository/EmpathyRepository.java
@@ -0,0 +1,8 @@
+package mjuphotolab.photolabbe.domain.empathy.repository;
+
+import mjuphotolab.photolabbe.domain.empathy.entity.Empathy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpathyRepository extends JpaRepository<Empathy, Long> {
+
+}
