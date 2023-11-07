6b2b28423fbbfe19b29ba094cc65020b85a0539c
나경호 <gangho324@gmail.com>
2023-11-08 01:30:29 +0900

    refactor: Photo api 수정

--- src/main/java/mjuphotolab/photolabbe/domain/photo/controller/PhotoController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/photo/controller/PhotoController.java
@@ -1,24 +1,25 @@
 package mjuphotolab.photolabbe.domain.photo.controller;
 
 import lombok.RequiredArgsConstructor;
-import mjuphotolab.photolabbe.domain.photo.entity.Photo;
+import mjuphotolab.photolabbe.domain.photo.controller.dto.response.PhotoDto;
 import mjuphotolab.photolabbe.domain.photo.service.PhotoService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("api/photos")
 @RequiredArgsConstructor
 public class PhotoController {
 
     private final PhotoService photoService;
 
-    @GetMapping("/photo/{photoId}")
-    public Photo getPhoto(@PathVariable("photoId") Long photoId) {
-        return photoService.getPhoto(photoId);
-    }
-
     // TODO: S3 사용하여 업로드 구현
-    @PostMapping("/photo")
-    public void uploadPhoto(@RequestParam(value = "title") String title, @RequestParam(value = "description") String description, @RequestParam(value = "imagePath") String imagePath) {
-        photoService.uploadPhoto(title, description, imagePath);
+    // @PostMapping("/new")
+    // public void uploadPhoto(@RequestBody RegisterPhotoDto registerPhotoDto) {
+    //     photoService.uploadPhoto(registerPhotoDto);
+    // }
+
+    @GetMapping("{photoId}")
+    public PhotoDto getPhoto(@PathVariable("photoId") Long photoId) {
+        return photoService.getPhoto(photoId);
     }
 }
