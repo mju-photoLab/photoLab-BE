8ab66f952d011d87d7a55d125738733f9c946c9b
나경호 <gangho324@gmail.com>
2023-11-09 14:44:17 +0900

    feat: 학번으로 조회하는 기능 추가

--- src/main/java/mjuphotolab/photolabbe/domain/user/repository/UserRepository.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/repository/UserRepository.java
@@ -18,4 +18,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByRefreshToken(String refreshToken);
 
 	Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String id);
+
+	Optional<User> findByStudentNumber(String studentNumber);
 }
