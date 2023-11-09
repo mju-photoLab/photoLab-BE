d90211e6507cd326898e594b6b8ba856b7d4b2c7
나경호 <gangho324@gmail.com>
2023-11-09 14:44:40 +0900

    feat: userpage 찾는 로직 생성

--- src/main/java/mjuphotolab/photolabbe/domain/user/service/UserService.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package mjuphotolab.photolabbe.domain.user.service;
 
+import java.util.List;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,19 +9,24 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mjuphotolab.photolabbe.auth.controller.dto.request.UserSignUpRequest;
+import mjuphotolab.photolabbe.domain.photo.controller.dto.response.AwardPhotoDto;
+import mjuphotolab.photolabbe.domain.user.controller.dto.response.UserPageResponse;
+import mjuphotolab.photolabbe.domain.user.controller.dto.response.UserProfileDto;
 import mjuphotolab.photolabbe.domain.user.entity.Role;
 import mjuphotolab.photolabbe.domain.user.entity.User;
 import mjuphotolab.photolabbe.domain.user.repository.UserRepository;
 
 @Service
 @Slf4j
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
+	// private final ExhibitionService exhibitionService;
 	private final PasswordEncoder passwordEncoder;
 
+	@Transactional
 	public void signUp(UserSignUpRequest userSignUpRequest) {
 		if (userRepository.findByEmail(userSignUpRequest.getEmail()).isPresent()) {
 			throw new IllegalArgumentException("[Error] 이미 존재하는 이메일입니다.");
@@ -38,14 +45,31 @@ public class UserService {
 		log.info("회원 저장 성공");
 	}
 
-	@Transactional(readOnly = true)
-	public User findUser(final String oauthId) {
-		return userRepository.findByOauthId(oauthId)
+	public User findUser(final Long userId) {
+		return userRepository.findById(userId)
 			.orElseThrow(() -> new IllegalArgumentException("[Error] 사용자를 찾을 수 없습니다."));
 	}
 
+	public UserProfileDto findUserProfile(User user) {
+		return UserProfileDto.of(user);
+	}
+
+	public User findByStudentNumber(final String studentNumber) {
+		return userRepository.findByStudentNumber(studentNumber)
+			.orElseThrow(() -> new IllegalArgumentException("[Error] 사용자를 찾을 수 없습니다."));
+	}
+
+	// TODO 마이페이지 찾는 로직
+	// public UserPageResponse findMyPage(Long userId) {
+	// 	User user = findUser(userId);
+	// 	List<AwardPhotoDto> awardPhotoDtos = exhibitionService.findByStudentNUmber(user.getStudentNumber());
+	//
+	// 	return UserPageResponse.from(user, awardPhotoDtos);
+	// }
+
+	@Transactional
 	public Long changeRole(final Long userId) {
-		User user =  userRepository.findById(userId)
+		User user = userRepository.findById(userId)
 			.orElseThrow(() -> new IllegalArgumentException("[Error] 사용자를 찾을 수 없습니다."));
 		if (!isAdmin(user)) {
 			user.updateRole(Role.ADMIN);
