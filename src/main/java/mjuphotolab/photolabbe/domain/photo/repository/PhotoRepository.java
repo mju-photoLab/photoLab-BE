16e1d6329944921d7066ed7db86ddb330eb6ac7f
나경호 <gangho324@gmail.com>
2023-11-12 00:58:23 +0900

    refactor: 좋아요 많은 사진 가져오기

--- src/main/java/mjuphotolab/photolabbe/domain/photo/repository/PhotoRepository.java
+++ src/main/java/mjuphotolab/photolabbe/domain/photo/repository/PhotoRepository.java
@@ -1,7 +1,14 @@
 package mjuphotolab.photolabbe.domain.photo.repository;
+import java.util.List;
 
 import mjuphotolab.photolabbe.domain.photo.entity.Photo;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+	@Query("SELECT p FROM Photo p ORDER BY p.likeCount DESC")
+	List<Photo> getBestPhotos(Pageable pageable);
 }
