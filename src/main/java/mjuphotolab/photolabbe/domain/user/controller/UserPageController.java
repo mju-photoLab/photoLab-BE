ff7ca49e0472d46686167c1aa955f7c10a72ed38
나경호 <gangho324@gmail.com>
2023-11-12 01:04:05 +0900

    feat: 유저 수정 api 추가

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserPageController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/UserPageController.java
@@ -1,20 +1,36 @@
 package mjuphotolab.photolabbe.domain.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import mjuphotolab.photolabbe.common.LoginUser;
+import mjuphotolab.photolabbe.domain.user.controller.dto.request.UpdateUserRequest;
+import mjuphotolab.photolabbe.domain.user.controller.dto.response.UserPageResponse;
+import mjuphotolab.photolabbe.domain.user.entity.User;
 import mjuphotolab.photolabbe.domain.user.service.UserService;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("api/users/")
+@RequestMapping("api/users/mypage")
 public class UserPageController {
 
 	private final UserService userService;
 
-	// @GetMapping("/mypage")
+	// @GetMapping
+	// @ResponseBody
 	// public UserPageResponse myPage(@LoginUser User user) {
 	// 	return userService.findMyPage(user.getId());
 	// }
+
+	@PatchMapping("/changeInfo")
+	public String changeInfo(@LoginUser User user, @RequestBody UpdateUserRequest updateUserRequest) {
+		userService.updateUser(user.getId(), updateUserRequest);
+		return "redirect:/api/users/mypage";
+	}
 }
