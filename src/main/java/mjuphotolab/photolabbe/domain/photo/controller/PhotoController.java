35996c438d3828ba5dd4d21e2d539cdd3c299ac9
나경호 <gangho324@gmail.com>
2023-11-13 19:34:19 +0900

    refactor: 잘못된 url 변경

--- src/main/java/mjuphotolab/photolabbe/domain/photo/controller/PhotoController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/photo/controller/PhotoController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import mjuphotolab.photolabbe.common.LoginUser;
 import mjuphotolab.photolabbe.domain.photo.controller.dto.response.BestPhotoDto;
 import mjuphotolab.photolabbe.domain.photo.controller.dto.response.PhotoDto;
 import mjuphotolab.photolabbe.domain.photo.service.PhotoService;
-
-
+import mjuphotolab.photolabbe.domain.user.entity.User;
 
 @RestController
 @RequestMapping("api/photos")
@@ -21,12 +21,12 @@ public class PhotoController {
 
     private final PhotoService photoService;
 
-    @GetMapping("{photoId}")
-    public PhotoDto getPhoto(@PathVariable("photoId") Long photoId) {
-        return photoService.findPhoto(photoId);
+    @GetMapping("/{photoId}")
+    public PhotoDto getPhoto(@PathVariable("photoId") Long photoId, @LoginUser User user) {
+        return photoService.findPhoto(photoId, user);
     }
 
-    @GetMapping("bestPhoto")
+    @GetMapping("/bestPhoto")
     public List<BestPhotoDto> getBestPhotos() {
         return photoService.findBestPhotos();
     }
