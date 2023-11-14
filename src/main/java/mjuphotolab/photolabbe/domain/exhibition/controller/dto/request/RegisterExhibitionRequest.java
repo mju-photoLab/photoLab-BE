70aa1d20316c2247a83dfdf0336464c9018733be
나경호 <gangho324@gmail.com>
2023-11-14 15:13:35 +0900

    refactor: 필요없는 필드값 제거

--- src/main/java/mjuphotolab/photolabbe/domain/exhibition/controller/dto/request/RegisterExhibitionRequest.java
+++ src/main/java/mjuphotolab/photolabbe/domain/exhibition/controller/dto/request/RegisterExhibitionRequest.java
@@ -1,25 +1,20 @@
 package mjuphotolab.photolabbe.domain.exhibition.controller.dto.request;
 
 import lombok.Getter;
-import mjuphotolab.photolabbe.domain.exhibition.controller.dto.response.ExhibitionDto;
 import mjuphotolab.photolabbe.domain.exhibition.entity.Exhibition;
-import mjuphotolab.photolabbe.domain.user.entity.User;
-
-import java.util.List;
+import mjuphotolab.photolabbe.domain.user.entity.User;;
 
 @Getter
 public class RegisterExhibitionRequest {
 
-    private User user;
-    private String title;
-    private String content;
-    private List<ExhibitionPhotoDto> exhibitionphotoDtos;
+	private String exhibitionTitle;
+	private String exhibitionContent;
 
-    public Exhibition toEntity(User user) {
-        return Exhibition.builder()
-                .user(user)
-                .title(title)
-                .content(content)
-                .build();
-    }
+	public Exhibition toEntity(User user) {
+		return Exhibition.builder()
+			.title(exhibitionTitle)
+			.content(exhibitionContent)
+			.user(user)
+			.build();
+	}
 }
