bdf6e977841f0f4d570f2b1f0f57c8a6b2dcb8da
나경호 <gangho324@gmail.com>
2023-11-14 15:05:36 +0900

    refactor: code convention

--- src/main/java/mjuphotolab/photolabbe/domain/exhibition/controller/dto/response/ExhibitionAllResponse.java
+++ src/main/java/mjuphotolab/photolabbe/domain/exhibition/controller/dto/response/ExhibitionAllResponse.java
@@ -7,19 +7,19 @@ import java.util.List;
 
 @Getter
 public class ExhibitionAllResponse {
-    private final int exhibitionsCount;
-    private final List<ExhibitionDto> exhibitionDtos;
+	private final int exhibitionsCount;
+	private final List<ExhibitionDto> exhibitionDtos;
 
-    @Builder
-    public ExhibitionAllResponse(final int exhibitionsCount, final List<ExhibitionDto> exhibitionDtos) {
-        this.exhibitionsCount = exhibitionsCount;
-        this.exhibitionDtos = exhibitionDtos;
-    }
+	@Builder
+	private ExhibitionAllResponse(final int exhibitionsCount, final List<ExhibitionDto> exhibitionDtos) {
+		this.exhibitionsCount = exhibitionsCount;
+		this.exhibitionDtos = exhibitionDtos;
+	}
 
-    public static ExhibitionAllResponse of(List<ExhibitionDto> exhibitionDtos) {
-        return ExhibitionAllResponse.builder()
-                .exhibitionsCount(exhibitionDtos.size())
-                .exhibitionDtos(exhibitionDtos)
-                .build();
-    }
+	public static ExhibitionAllResponse of(List<ExhibitionDto> exhibitionDtos) {
+		return ExhibitionAllResponse.builder()
+			.exhibitionsCount(exhibitionDtos.size())
+			.exhibitionDtos(exhibitionDtos)
+			.build();
+	}
 }
