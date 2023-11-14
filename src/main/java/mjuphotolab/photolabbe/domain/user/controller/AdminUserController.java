b92b9ab7da59788e89031875b0335784ec26ebf7
나경호 <gangho324@gmail.com>
2023-11-14 14:55:28 +0900

    feat: 유저 전체 조회

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/AdminUserController.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/AdminUserController.java
@@ -1,12 +1,15 @@
 package mjuphotolab.photolabbe.domain.user.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import mjuphotolab.photolabbe.common.LoginUser;
+import mjuphotolab.photolabbe.common.meta.LoginUser;
+import mjuphotolab.photolabbe.domain.user.controller.dto.response.UserResponse;
 import mjuphotolab.photolabbe.domain.user.entity.User;
 import mjuphotolab.photolabbe.domain.user.service.UserService;
 
@@ -16,9 +19,15 @@ import mjuphotolab.photolabbe.domain.user.service.UserService;
 public class AdminUserController {
 
 	private final UserService userService;
+
 	@PatchMapping("/editRole")
 	public Long updateRoleToAdmin(@LoginUser User user) {
 		return userService.changeRole(user.getId());
+	}
+
+	@GetMapping
+	public List<UserResponse> findAllUsers() {
+		return userService.findAll();
 	}
 
 	@GetMapping("/jwt-test")
