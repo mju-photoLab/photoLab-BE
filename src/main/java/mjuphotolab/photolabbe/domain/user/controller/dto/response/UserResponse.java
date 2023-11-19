a38ef0d6f79ec0038e914bf637f02cd09214192f
나경호 <gangho324@gmail.com>
2023-11-19 23:32:00 +0900

    refactor: 학번 필드 추가

--- src/main/java/mjuphotolab/photolabbe/domain/user/controller/dto/response/UserResponse.java
+++ src/main/java/mjuphotolab/photolabbe/domain/user/controller/dto/response/UserResponse.java
@@ -8,17 +8,21 @@ import mjuphotolab.photolabbe.domain.user.entity.User;
 public class UserResponse {
 	private final String nickname;
 	private final String role;
+	private final String studentNumber;
+
 
 	@Builder
-	private UserResponse(final String nickname, final String role) {
+	private UserResponse(final String nickname, final String role, final String studentNumber) {
 		this.nickname = nickname;
 		this.role = role;
+		this.studentNumber = studentNumber;
 	}
 
 	public static UserResponse of(User user) {
 		return UserResponse.builder()
 			.nickname(user.getNickname())
 			.role(user.getRole().name())
+			.studentNumber(user.getStudentNumber())
 			.build();
 	}
 }
