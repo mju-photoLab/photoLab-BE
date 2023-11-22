dbc6712fba7cfbe1c51f1642356a5d8c4c82fdb8
나경호 <gangho324@gmail.com>
2023-11-22 15:59:48 +0900

    chore: mysql 설정

--- src/main/java/mjuphotolab/photolabbe/auth/jwt/service/JwtService.java
+++ src/main/java/mjuphotolab/photolabbe/auth/jwt/service/JwtService.java
@@ -4,9 +4,6 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
