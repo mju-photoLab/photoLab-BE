75d38c2c76f39b147a258b595034be96c9ec3394
나경호 <gangho324@gmail.com>
2023-11-28 12:54:48 +0900

    refactor: cors 전역 설정 (#86)
    * chore: runtimeOnly 수정
    
    * feat: HealthCheckController 생성
    
    * chore: 배포 설정
    
    * chore: oauth 설정
    
    * chore: update deploy.yml
    
    * chore: update deploy.yml
    
    * chore: update deploy.yml
    
    * chore: update deploy.yml
    
    * chore: update build.gradle
    
    * test: 제외
    
    * refactor: aws 환경변수 수정
    
    * refactor: aws 환경변수 수정
    
    * chore: kakao oauth ip 변경
    
    * chore: db 설정 수정
    
      jpa:
        properties:
          hibernate:
            default_schema: photolab
    
    * chore: db 설정 수정
    
    * feat: cors 전역 설정
    
    * feat: cors 전역 설정

--- src/main/java/mjuphotolab/photolabbe/common/config/WebConfig.java
+++ src/main/java/mjuphotolab/photolabbe/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOrigins("http://localhost:3000")
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("Authorization", "Content-Type")
 			.exposedHeaders("Custom-Header")
